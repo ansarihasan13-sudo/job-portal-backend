4592a6b6ed9d9845fbed68bbf02be27f47179389
Rashid <ansarihasan13@gmail.com>
2026-09-19 12:57:47 +0530

    Fix production CORS

--- src/main/java/com/jobsportal/config/SecurityConfig.java
+++ src/main/java/com/jobsportal/config/SecurityConfig.java
@@ -64,7 +64,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://job-portal-frontend-9etr.vercel.app"
+        ));
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );
